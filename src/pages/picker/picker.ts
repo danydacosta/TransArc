07734c78734b3129d871af1d6fbbec54ee62ada1
danydc@comac.ch <danydc@comac.ch>
2017-10-31 12:06:54 +0100

    Rename la HomePage en PickerPage

--- src/pages/picker/picker.ts
+++ src/pages/picker/picker.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: 'page-picker',
+  templateUrl: 'picker.html'
 })
-export class HomePage {
+export class PickerPage {
 
   constructor(public navCtrl: NavController) {
 
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -2,14 +2,14 @@ import { Component } from '@angular/core';
 
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
+import { PickerPage } from '../picker/picker';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
+  tab1Root = PickerPage;
   tab2Root = AboutPage;
   tab3Root = ContactPage;
 
